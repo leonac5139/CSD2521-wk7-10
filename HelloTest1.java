5025dd7a86bb9c4e03f728dbcf06be1ce943f875
leonac5139 <70214297+leonac5139@users.noreply.github.com>
2020-10-09 12:52:58 -0400

    Update HelloTest1.java
    I added an update on line 3 and modified the comments on line 8.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10/9/20 CAL
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull Request.
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
